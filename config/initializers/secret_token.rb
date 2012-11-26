587f83ef583c9fd7e59de5a4ca44b86112a922ba
Alan Cohen <alan.mit@gmail.com>
2012-11-26 14:44:34 -0800

    Initial commit. Empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hangyourboots::Application.config.secret_token = '814cb887c9ffc7e4ecef1de5a8dad77d73ebf8ac628eb40e665b10d365533291e690b5e730e7f9ffc108f676ff702e198bf0b8572e30d234764b77db922218c8'
