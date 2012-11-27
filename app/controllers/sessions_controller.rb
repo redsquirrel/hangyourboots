505c120bc23b2a8e9950eba767631c315fd440ed
Alan Cohen, Jonathan Pepin <alan.mit@gmail.com, jypepin@gmail.com>
2012-11-27 12:02:44 -0800

    Make User model and sessions controller ready for OmniAuth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    raise request.env["omniauth.auth"].to_yaml
+    # Use 'raise' to check the information Twitter provides to us in the request
+    # raise request.env["omniauth.auth"].to_yaml
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :email, :gender
   validates_uniqueness_of :email, :case_sensitive => false
 
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["user_info"]["name"]
+    end
+  end
 end
