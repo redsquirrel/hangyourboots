73d0299165de1825f1fe1a3315e2ef4c3a4f4a1b
Alan Cohen <alan.mit@gmail.com>
2012-11-26 16:15:36 -0800

    Update migrations via instructions from Tanner

--- db/migrate/20121126231945_create_users_table.rb
+++ db/migrate/20121126231945_create_users_table.rb
@@ -1,8 +1,8 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :name, :null => false
+      t.string :email, :null => false
       t.string :gender
       t.text  :bio
       t.timestamps
--- db/migrate/20121126231954_create_houses_table.rb
+++ db/migrate/20121126231954_create_houses_table.rb
@@ -2,14 +2,13 @@ class CreateHousesTable < ActiveRecord::Migration
   def change
     create_table :houses do |t|
       t.string :title
-      t.string :address
+      t.string :address, :null => false
       t.text :description
       t.string :maps_link
       t.integer :rooms
       t.integer :beds
       t.integer :capacity
-      t.integer :total_cost
-      t.integer :distance_miles
+      t.integer :total_cost, :null => false
 
       t.timestamps
     end
