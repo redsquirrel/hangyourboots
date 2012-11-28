9cc3bfd42c67d2de4299394230cae01c34845dca
Jeremy James <jeremyleejames@gmail.com>
2012-11-28 10:40:00 -0800

    add associations for users and houses

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,7 +3,9 @@ attr_accessible :title, :address, :description, :maps_link, :rooms,
 								:beds, :bathrooms, :capacity, :total_cost, :image
 
 validates :title, :address, :description, :maps_link,
-					:rooms, :beds, :bathrooms, :capacity, :total_cost, :image, 		:presence => true
+					:rooms, :beds, :bathrooms, :capacity, :total_cost, 		:presence => true
 
 mount_uploader :image, ImageUploader
+has_many :commitments
+has_many :users, :through => :commitments
 end
