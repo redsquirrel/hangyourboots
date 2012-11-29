985e1260b193131a08389aa862f2f9834e56748c
Andrew Fowler, Amy Lai <fowler446@hotmail.com, amy.jh.lai@gmail.com>
2012-11-29 15:11:34 -0800

    remove current_user= method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
-  def current_user=(user)
-    @current_user = user
-  end
 end
