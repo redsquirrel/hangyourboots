477896054419e4d0fa86fa26874078992f96582b
Alan Cohen, Jonathan Pepin <alan.mit@gmail.com, jypepin@gmail.com>
2012-11-29 16:38:11 -0800

    Complete user and houses controller authorization specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
   respond_to :html
-
   # Only allow users or admins to edit, update, destroy accounts
-  before_filter :authorize_user, :only => [:edit, :update, :destroy]
+  before_filter :load_and_authorize_user, :only => [:edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -15,17 +14,14 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update_attributes(params[:user])
     respond_with(@user)
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     redirect_to users_path
   end
@@ -39,7 +35,8 @@ class UsersController < ApplicationController
   end
 
   private
-  def authorize_user
+  def load_and_authorize_user
+    @user = User.find(params[:id])
     redirect_to root_url unless current_user == @user || current_user.is_admin?
   end
 end
--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -2,76 +2,117 @@ require 'spec_helper'
 
 describe HousesController do
 
-  # Skip authorization for the unit controller tests
-  HousesController.send(:skip_before_filter,:authorize_user)
+  shared_examples("member access to houses") do
+    describe 'GET #index' do
+      it "gets all the houses in the database" do
+        house = FactoryGirl.create(:house)
+        get :index
+        assigns(:houses).should eq [house]
+      end
+    end #GET index
 
-  describe 'GET #index' do
-    it "gets all the houses in the database" do
-      house = FactoryGirl.create(:house)
-      get :index
-      assigns(:houses).should eq [house]
+    describe "GET #show" do
+      it "assigns the requested house to @house" do
+        house = FactoryGirl.create(:house)
+        get :show, id: house
+        assigns(:house).should eq house
+      end
+    end #@GET show
+  end
+
+  shared_examples("unauthorized access to houses") do
+    describe "#create" do 
+      before {post :create, house: FactoryGirl.attributes_for(:house)}
+      it { should redirect_to root_url }
     end
-  end #GET index
 
-  describe "GET #show" do
-    it "assigns the requested house to @house" do
-      house = FactoryGirl.create(:house)
-      get :show, id: house
-      assigns(:house).should eq house
+    describe "#edit" do 
+      before do 
+        house = FactoryGirl.create(:house)
+        get :edit, id: house.id
+      end
+      it { should redirect_to root_url }
     end
-  end #@GET show
+    
+    describe "#update" do 
+      before do 
+        house = FactoryGirl.create(:house)
+        put :update, id: house.id, house: FactoryGirl.attributes_for(:house)
+      end
+      it { should redirect_to root_url }
+    end
+    
+    describe "#destroy" do 
+      before do 
+        house = FactoryGirl.create(:house)
+        delete :destroy, id: house.id
+      end
+      it { should redirect_to root_url }
+    end
+  end
 
-  describe "POST #create" do
+  shared_examples("admin access to houses") do
+    describe "POST #create" do
+      context "with valid attributes" do
+        it "saves the new house in the database" do
+          expect {
+            post :create, house: FactoryGirl.attributes_for(:house)
+          }.to change(House, :count).by(1)
+        end
 
-    context "with valid attributes" do
-
-      it "saves the new house in the database" do
-        expect {
+        it "redirects to the house show page" do
           post :create, house: FactoryGirl.attributes_for(:house)
-        }.to change(House, :count).by(1)
+          response.should redirect_to house_path(House.last.id)
+        end
+      end #valid attrs
 
-      end
+      context "with invalid attributes" do
+        it "does not save the new house in the database" do
+          expect {
+            post :create, house: FactoryGirl.attributes_for(:invalid_house)
+          }.to_not change(House, :count)
+        end
 
-      it "redirects to the house show page" do
-        post :create, house: FactoryGirl.attributes_for(:house)
-        response.should redirect_to house_path(House.last.id)
-      end
-    end #valid attrs
-
-    context "with invalid attributes" do
-      it "does not save the new house in the database" do
-        expect {
+        it "re-renders the :new template" do
           post :create, house: FactoryGirl.attributes_for(:invalid_house)
-        }.to_not change(House, :count)
-      end
+          response.should render_template :new
+        end
+      end #invalid attrs
+    end #POST create
 
-      it "re-renders the :new template" do
-        post :create, house: FactoryGirl.attributes_for(:invalid_house)
-        response.should render_template :new
-      end
-    end #invalid attrs
-  end #POST create
+    describe'PUT#update' do
 
-  describe'PUT#update' do
+      subject { FactoryGirl.create(:house) }
 
-    subject { FactoryGirl.create(:house) }
-
-    it "locates the requested house" do
-      put :update, id: subject, house: FactoryGirl.attributes_for(:house)
-      assigns(:house).should eq(subject)
-    end
-
-    context "valid attributes" do
-      it "changes house's attributes" do
-        put :update, id: subject, house: FactoryGirl.attributes_for(:house, title: "New title")
-        subject.reload
-        subject.title.should eq("New title")
-      end
-
-      it "redirects to the updated house" do
+      it "locates the requested house" do
         put :update, id: subject, house: FactoryGirl.attributes_for(:house)
-        response.should redirect_to subject
+        assigns(:house).should eq(subject)
+      end
+
+      context "valid attributes" do
+        it "changes house's attributes" do
+          put :update, id: subject, house: FactoryGirl.attributes_for(:house, title: "New title")
+          subject.reload
+          subject.title.should eq("New title")
+        end
+
+        it "redirects to the updated house" do
+          put :update, id: subject, house: FactoryGirl.attributes_for(:house)
+          response.should redirect_to subject
+        end
       end
     end
+  end
+
+  context "admin access to houses" do
+    before(:each) {set_user_session create(:admin_user)}
+    it_behaves_like "member access to houses"
+    it_behaves_like "admin access to houses"
+  end
+
+  context "non-admin access to houses" do
+    before(:each) {set_user_session create(:user)}
+    it_behaves_like "member access to houses"
+    it_behaves_like "unauthorized access to houses"
   end
 end
