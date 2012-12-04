033b441dd89dc359fd1fa435f7d26849ab902b2f
Alan Cohen <alan.mit@gmail.com>
2012-12-04 10:37:08 -0800

    Fix the bugs I added to the mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,14 @@
 class UserMailer < ActionMailer::Base
   default :from => "hangyourbootsapp@gmail.com"
 
+  def house_is_full_email!(users)
+    users.each do |user|
+      UserMailer.house_confirmation!(user).deliver
+    end
+  end
+
   def house_confirmation!(user)
     email_with_name = "#{user.name} <#{user.email}>"
     mail(:to => email_with_name, :subject => "Your house is complete!")
-  end
-
-  def house_is_full_email!
-    current_user.roommates.each do |user|
-      UserMailer.house_confirmation!(user).deliver
-    end
   end
 end
