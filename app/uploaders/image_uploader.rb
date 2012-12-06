93eb7899428abfc2b238b20431707522c14f33d9
Andrew <addy.mfowfow@gmail.com>
2012-12-05 17:38:08 -0800

    config carrierwave to user s3 and heroku environment variables

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
-  # storage :fog
+
+  include CarrierWave::MimeTypes
+  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
