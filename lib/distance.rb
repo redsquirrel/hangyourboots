7ed4fe68e2f3dd6d8f5f2ee11a390c18cda1319d
Dave Hoover <dave.hoover@gmail.com>
2013-01-31 14:57:21 -0600

    Updating Distance calculator to use Chicago

--- lib/distance.rb
+++ lib/distance.rb
@@ -1,17 +1,42 @@
 require 'httparty'
 
 module Distance
+  extend self
 
-  def self.calculate(address)
-    dbc_address = '717 California St, San Francisco, CA'.split(' ').join('%20')
-    address = address.split(' ').join('%20')
-    result = HTTParty.get("http://maps.googleapis.com/maps/api/distancematrix/json?origins=#{address}&destinations=#{dbc_address}&mode=walking&units=imperial&sensor=false")
+  DBC_LOCATIONS = ['717 California St, San Francisco, CA', '414 N. Orleans St, Chicago, IL']
+
+  def calculate(address)
+    closest_address = min_distance = result = nil
+
+    DBC_LOCATIONS.each do |dbc_address|
+      dbc_address = prepare_address(dbc_address)
+      address = prepare_address(address)
+
+      result = HTTParty.get("http://maps.googleapis.com/maps/api/distancematrix/json?origins=#{address}&destinations=#{dbc_address}&mode=walking&units=imperial&sensor=false")
+
+      distance = to_distance(result).to_i
+      min_distance ||= distance
+      if distance < min_distance
+        min_distance = distance
+        closest_address = dbc_address
+      end
+    end
 
     return {
-          :distance => result["rows"][0]["elements"][0]["distance"]["text"],
-          :duration => result["rows"][0]["elements"][0]["duration"]["text"]
-          }
-
+      :distance => to_distance(result),
+      :duration => to_duration(result)
+    }
   end
 
+  def to_distance(result)
+    result["rows"][0]["elements"][0]["distance"]["text"]
+  end
+
+  def to_duration(result)
+    result["rows"][0]["elements"][0]["duration"]["text"]
+  end
+
+  def prepare_address(raw)
+    raw.split(' ').join('%20')
+  end
 end
